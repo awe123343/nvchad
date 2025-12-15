43257e47dca14af1348ed0138403a4053825882f
awe123343 <awe1233430@hotmail.com>
2025-12-14 23:51:12 -0800

    chore(python): use main for VenvSelect

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -11,7 +11,6 @@ return {
   {
     "linux-cultist/venv-selector.nvim",
     dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
-    branch = "regexp",
     opts = {
       -- stay_on_this_version = true,
       name = { "snapc_env", ".venv" },
