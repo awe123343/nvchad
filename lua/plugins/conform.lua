8ae8a9adb72147fa5c6686226eb3735f3715a89b
awe123343 <awe1233430@hotmail.com>
2025-12-14 23:51:12 -0800

    feat(conform): support disable auto-format

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -53,11 +53,35 @@ return {
     -- other conform config
     local conform = require "conform"
     conform.setup {
-      format_on_save = {
-        lsp_fallback = true,
-        timeout_ms = 5000,
-      },
+      format_on_save = function(bufnr)
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return {
+          lsp_fallback = true,
+          timeout_ms = 5000,
+        }
+      end,
     }
+
+    vim.api.nvim_create_user_command("FormatDisable", function(args)
+      if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.disable_autoformat = true
+      else
+        vim.g.disable_autoformat = true
+      end
+    end, {
+      desc = "Disable autoformat-on-save",
+      bang = true,
+    })
+    vim.api.nvim_create_user_command("FormatEnable", function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, {
+      desc = "Enable autoformat-on-save",
+    })
+
     vim.keymap.set({ "n", "v" }, "<leader>lf", function()
       conform.format {
         lsp_fallback = true,
