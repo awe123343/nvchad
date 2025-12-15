d284e573a5344169c333abf62d3094b3a9e6a70f
awe123343 <awe1233430@hotmail.com>
2025-12-14 23:51:12 -0800

    feat(lsp): vtsls for js/ts

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -43,6 +43,7 @@ keymap("n", "q", "<cmd>q<cr>", opts)
 
 -- DAP Core
 vim.keymap.set("n", "<leader>db", "<cmd> DapToggleBreakpoint <CR>", { desc = "Add breakpoint at line" })
+vim.keymap.set("n", "<leader>dr", "<cmd> DapContinue <CR>", { desc = "Start or continue the debugger" })
 vim.keymap.set("n", "<leader>dus", function()
   local widgets = require "dap.ui.widgets"
   local sidebar = widgets.sidebar(widgets.scopes)
--- lua/plugins/js.lua
+++ lua/plugins/js.lua
@@ -1,9 +1,162 @@
 return {
   {
     "windwp/nvim-ts-autotag",
-    ft = {"javascript", "javascriptreact", "typescript", "typescriptreact"},
+    ft = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
     config = function()
       require("nvim-ts-autotag").setup()
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "js-debug-adapter",
+        "prettier",
+        "vtsls",
+        "eslint-lsp",
+        "html-lsp",
+        "css-lsp",
+        "json-lsp",
+      },
+    },
+  },
+  {
+    "pojokcodeid/auto-lsp.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      -- Use vtsls instead of ts_ls/tsserver
+      vim.list_extend(opts.ensure_installed, { "vtsls", "eslint", "html", "cssls", "jsonls" })
+    end,
+    init = function()
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = vim.api.nvim_create_augroup("TS_OrganizeImports", { clear = true }),
+        pattern = { "*.ts", "*.tsx", "*.js", "*.jsx" },
+        callback = function()
+          pcall(vim.lsp.buf.execute_command, {
+            command = "source.organizeImports",
+            arguments = { vim.api.nvim_buf_get_name(0) },
+          })
+        end,
+      })
+    end,
+    config = function(_, opts)
+      require("auto-lsp").setup(opts)
+
+      -- Specific configuration for vtsls
+      local lspconfig = require "lspconfig"
+      lspconfig.vtsls.setup {
+        -- Make sure to attach your standard on_attach and capabilities here if needed
+        -- The auto-lsp plugin usually handles this, but vtsls specific settings go here:
+        settings = {
+          vtsls = {
+            autoUseWorkspaceTsdk = true,
+            experimental = {
+              completion = {
+                enableServerSideFuzzyMatch = true,
+              },
+            },
+          },
+          typescript = {
+            updateImportsOnFileMove = { enabled = "always" },
+            suggest = {
+              completeFunctionCalls = true,
+            },
+            inlayHints = {
+              parameterNames = { enabled = "literals" },
+              parameterTypes = { enabled = true },
+              variableTypes = { enabled = false },
+              propertyDeclarationTypes = { enabled = true },
+              functionLikeReturnTypes = { enabled = true },
+              enumMemberValues = { enabled = true },
+            },
+          },
+          javascript = {
+            updateImportsOnFileMove = { enabled = "always" },
+            inlayHints = {
+              parameterNames = { enabled = "literals" },
+              parameterTypes = { enabled = true },
+              variableTypes = { enabled = false },
+              propertyDeclarationTypes = { enabled = true },
+              functionLikeReturnTypes = { enabled = true },
+              enumMemberValues = { enabled = true },
+            },
+          },
+        },
+      }
+    end,
+  },
+  {
+    "pojokcodeid/auto-conform.nvim",
+    opts = function(_, opts)
+      opts.formatters_by_ft = opts.formatters_by_ft or {}
+      local js_formatters = { "prettier" }
+      opts.formatters_by_ft["javascript"] = js_formatters
+      opts.formatters_by_ft["typescript"] = js_formatters
+      opts.formatters_by_ft["javascriptreact"] = js_formatters
+      opts.formatters_by_ft["typescriptreact"] = js_formatters
+      opts.formatters_by_ft["json"] = { "prettier" }
+      opts.formatters_by_ft["html"] = { "prettier" }
+      opts.formatters_by_ft["css"] = { "prettier" }
+      opts.formatters_by_ft["scss"] = { "prettier" }
+
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "prettier" })
+    end,
+  },
+  {
+    "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require "dap"
+
+      -- Adapter for Node.js
+      if not dap.adapters["pwa-node"] then
+        dap.adapters["pwa-node"] = {
+          type = "server",
+          host = "127.0.0.1",
+          port = "${port}",
+          executable = {
+            command = "js-debug-adapter",
+            args = { "${port}" },
+          },
+        }
+      end
+
+      -- Adapter for Chrome (Frontend)
+      if not dap.adapters["pwa-chrome"] then
+        dap.adapters["pwa-chrome"] = {
+          type = "server",
+          host = "127.0.0.1",
+          port = "${port}",
+          executable = {
+            command = "js-debug-adapter",
+            args = { "${port}" },
+          },
+        }
+      end
+
+      for _, language in ipairs { "typescript", "javascript", "javascriptreact", "typescriptreact" } do
+        if not dap.configurations[language] then
+          dap.configurations[language] = {
+            -- Debug Node.js (Backend/Scripts)
+            {
+              type = "pwa-node",
+              request = "launch",
+              name = "Launch Current File (pwa-node)",
+              program = "${file}",
+              cwd = "${workspaceFolder}",
+            },
+            -- Debug Chrome (Frontend)
+            {
+              type = "pwa-chrome",
+              request = "launch",
+              name = "Launch Chrome with 'localhost'",
+              url = "http://localhost:3000", -- Default for React, change to 5173 for Vite/Vue if needed
+              webRoot = "${workspaceFolder}",
+              userDataDir = "${workspaceFolder}/.vscode/vscode-chrome-profile",
+            },
+          }
+        end
+      end
     end,
   },
 }
