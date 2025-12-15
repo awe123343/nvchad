038f0c4bf11e6b2315d959849e7521d60aa3c54f
awe123343 <awe1233430@hotmail.com>
2025-12-14 23:51:12 -0800

    chore(csharp): ensure lsp

--- lua/plugins/csharp.lua
+++ lua/plugins/csharp.lua
@@ -1,12 +1,21 @@
 return {
-  "iabdelkareem/csharp.nvim",
-  dependencies = {
-    "williamboman/mason.nvim", -- Required, automatically installs omnisharp
-    "mfussenegger/nvim-dap",
-    "Tastyep/structlog.nvim", -- Optional, but highly recommended for debugging
+  {
+    "iabdelkareem/csharp.nvim",
+    dependencies = {
+      "williamboman/mason.nvim", -- Required, automatically installs omnisharp
+      "mfussenegger/nvim-dap",
+      "Tastyep/structlog.nvim", -- Optional, but highly recommended for debugging
+    },
+    config = function()
+      require("mason").setup() -- Mason setup must run before csharp, only if you want to use omnisharp
+      require("csharp").setup()
+    end,
   },
-  config = function()
-    require("mason").setup() -- Mason setup must run before csharp, only if you want to use omnisharp
-    require("csharp").setup()
-  end,
+  {
+    "pojokcodeid/auto-lsp.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "csharp_ls", "omnisharp" })
+    end,
+  },
 }
