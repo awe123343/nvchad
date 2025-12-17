2b13aded10be76ba017916e87f4c9d1ff73205b2
awe123343 <awe1233430@hotmail.com>
2025-12-17 01:13:37 -0800

    feat(git): gitsigns and nvim-diffview

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -2,22 +2,47 @@ return {
   {
     "tpope/vim-fugitive",
     event = "VeryLazy",
+    dependencies = {
+      "tpope/vim-rhubarb", -- For :GBrowse
+    },
   },
   {
-    "f-person/git-blame.nvim",
-    -- load the plugin at startup
-    event = "VeryLazy",
-    -- Because of the keys part, you will be lazy loading this plugin.
-    -- The plugin will only load once one of the keys is used.
-    -- If you want to load the plugin at startup, add something like event = "VeryLazy",
-    -- or lazy = false. One of both options will work.
+    "lewis6991/gitsigns.nvim",
     opts = {
-      -- your configuration comes here
-      -- for example
-      enabled = true, -- if you want to enable the plugin
-      message_template = " <author> • <date> • <summary> • <<sha>>", -- template for the blame message, check the Message template section for more options
-      date_format = "%Y-%m-%d %H:%M:%S", -- template for the date, check Date format section for more options
-      virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
+      current_line_blame = true,
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = "eol",
+        delay = 200,
+      },
+      current_line_blame_formatter = " <author> • <author_time:%Y-%m-%d %H:%M:%S> • <summary> • <abbrev_sha>",
+    },
+  },
+  {
+    "sindrets/diffview.nvim",
+    cmd = {
+      "DiffviewOpen",
+      "DiffviewClose",
+      "DiffviewToggleFiles",
+      "DiffviewFocusFiles",
+    },
+    opts = {
+      enhanced_diff_hl = true,
+      view = {
+        default = { winbar_info = true },
+        file_history = { winbar_info = true },
+        merge_tool = {
+          layout = "diff3_horizontal",
+          disable_diagnostics = true,
+          winbar_info = true,
+        },
+      },
+      hooks = {
+        -- Prevents other UI plugins from messing up the diff view
+        diff_buf_read = function(bufnr)
+          vim.b[bufnr].view_activated = false
+        end,
+      },
     },
   },
 }
